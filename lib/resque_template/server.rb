3cb545ed844317eefb80bd041dbb2d117ac727f5
Steve Dierker <steve.dierker@flavoursys.com>
2012-08-03 19:48:34 +0200

    * we can just set the publicfolder..

--- lib/resque_template/server.rb
+++ lib/resque_template/server.rb
@@ -3,17 +3,8 @@ require 'resque/server'
 
 module ResqueTemplate
   module Server
-    path = File.dirname(File.expand_path(__FILE__))
-    # Set new public dir
-    if respond_to? :public_folder
-      set :public_folder, "#{path}/server/public"
-    else
-      set :public, "#{path}/server/public"
-    end
-
+  
   end
 end
-
-Resque::Server.class_eval do
-  include ResqueTemplate::Server
-end
+path = File.dirname(File.expand_path(__FILE__))
+Resque::Server.public_folder = "#{path}/server/public"
