63dbb5bf1cf1d5952562923331122c15fa97c6d6
Steve Dierker <steve.dierker@flavoursys.com>
2012-08-03 19:35:42 +0200

    * changed color of resque for test purpose * added definitions to change public path of resque

--- lib/resque_template/server.rb
+++ lib/resque_template/server.rb
@@ -3,10 +3,17 @@ require 'resque/server'
 
 module ResqueTemplate
   module Server
-    
+    path = File.dirname(File.expand_path(__FILE__))
+    # Set new public dir
+    if respond_to? :public_folder
+      set :public_folder, "#{path}/server/public"
+    else
+      set :public, "#{path}/server/public"
+    end
+
   end
 end
 
 Resque::Server.class_eval do
-  include ResqueScheduler::Server
+  include ResqueTemplate::Server
 end
